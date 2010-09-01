18884bb18fb65797fa83b5f655b39382ce04d800
s.zittermann <s.zittermann@ad4914cc-eacd-a80a-4db4-1cd973497436>
2010-09-01 11:45:37 +0000

    Gui-update

--- trunk/src/java/net/naproche/preparser/Example.java
+++ trunk/src/java/net/naproche/preparser/Example.java
@@ -1,13 +1,36 @@
 package net.naproche.preparser;
 import java.util.Hashtable;
 import java.util.LinkedList;
+import java.util.*;
 import jpl.Query;
 import jpl.*;
+import java.io.*;
 
 // Example: Calls create_naproche_input from Java and parses the returnvalue with convertSentences.
 
 public class Example{
 	public static void main(String args[]){
+		
+		/*
+		String dir="user.dir"; // set to current directory
+		 try {dir=new File(System.getProperty(dir)).getCanonicalPath();}
+		 catch (IOException e1) { 
+		 //handler required but null  
+		}
+		 System.out.println ("Current dir : " + dir);
+		 
+		 File test = new File("examples/mizar");
+		 String[] files = test.list();
+		 
+		 List scrambled = Arrays.asList(files);
+		 //Collections.sort(scrambled, new NaturalOrderComparator());
+		 
+		 System.out.println("Scrambled: " + scrambled);
+		 
+		 for (String file: files) {
+		         System.out.println(file);
+		 }
+		//*/
 		new Query("['src/prolog/load_jpl'].").oneSolution();
 		Query create_naproche_input =
 			// new Query("create_naproche_input('Let $n$ be in $\\\\mathbb{N}$. Then $n > 0$.',L).");
